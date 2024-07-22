e85fe6fd37e79447a2de02c1cc5a981b2d316862
jerodriguez <jerodriguez@powerstreet.com.mx>
2024-07-22 13:48:37 -0600

    removing perfil admin

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,14 @@
 import OptionsCard from '@/components/shared/OptionsCard'
 import React from 'react'
 import { mainPageOptions } from '@/constants'
+import { Button } from '@/components/ui/button'
 
+const clientJSON = process.env.GDRIVE_CLIENT_JSON
 
 const HomePage = () => {
   return (
     <div className='mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6'>
-      {mainPageOptions.map(option => (
-        <OptionsCard
-          title={option.title}
-          href={option.route}
-        />
-      ))}
+      <Button>Subir archivo</Button>
 
     </div>
   )
